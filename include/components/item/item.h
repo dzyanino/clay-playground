08d9c3903a227d7447f187b13fb1db32894ee07f
dzyanino <dzyanino@gmail.com>
2026-07-12 23:16:59 +0300

    feat(item): functional item with hover and variant

--- include/components/item/item.h
+++ include/components/item/item.h
@@ -4,6 +4,6 @@
 #include "../../theme/colors.h"
 #include "../../theme/variants.h"
 
-void ComponentItem(char *id, char *title, char *description, m_variant_id_t variant_id, m_color_id_t color_id, int *disabled, void (*callback)());
+void ComponentItem(char *id, char *title, char *description, char *bottom_left, char *bottom_right, m_variant_id_t variant_id, m_color_id_t color_id, int *disabled, void (*callback)());
 
 #endif
